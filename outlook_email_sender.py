701c02b5db080b9e6b0ffd931337ead49fc30e9c
Minusandhs <minusandhs@gmail.com>
2022-07-17 16:11:44 +0530

    The attachments will be added in a folder

--- outlook_email_sender.py
+++ outlook_email_sender.py
@@ -2,7 +2,7 @@ import win32com.client as win32
 
 
 class outlook_email_sender():
-    def __init__(self,mail_to,mail_subject,mail_body,mail_attachment=None,HTML = False):
+    def __init__(self,mail_to,mail_subject,mail_body,mail_attachment=None,HTML = False,mail_attachment_folder=None):
         self.olApp = win32.Dispatch('Outlook.Application')
         self.mail_item = self.olApp.CreateItem(0)
         self.mail_item.To = mail_to
@@ -10,21 +10,28 @@ class outlook_email_sender():
         self.attachment = mail_attachment
         self.is_html = HTML
         self.mail_body = mail_body
+        self.mail_attachment_folder = mail_attachment_folder
         
         #Check if there is a mail attachment and whether its str or list
         if self.attachment == None:
             pass
 
         elif self.attachment != None and isinstance(self.attachment,list):
-
-            for attach in self.attachment:
-                self.mail_item.Attachments.Add(attach)
-
+            
+            try:
+                for attach in self.attachment:
+                    self.mail_item.Attachments.Add(attach)
+            except:
+                raise ('File not found')
         elif (self.attachment != None and isinstance(self.attachment,str)):
-            self.mail_item.Attachments.Add(self.attachment)
+
+            try:
+                self.mail_item.Attachments.Add(self.attachment)
+            except:
+                raise ('File not found')
 
         else:
-            raise TypeError('The attachment path should in string or list')
+            raise ('The attachment path should in string or list')
         
         #Add the mail body by checking whether it is HTML
         if self.is_html:
@@ -32,6 +39,19 @@ class outlook_email_sender():
         else:
             self.mail_item.Body = self.mail_body
 
+        #If a folder path given as a parameter
+        if mail_attachment_folder == None:
+            pass
+        else:
+            try:
+                file_list = file_in_dir(self.mail_attachment_folder)
+                for attach in file_list:
+                    self.mail_item.Attachments.Add(attach)
+
+            except:
+                raise ("Please select a valid folder")
+
+    ##Check whether one object is equal to other object
     def __eq__(self,other):
         if (self.mail_item.To == other.mail_item.To) and \
             (self.mail_item.Subject == other.mail_item.Subject) and\
@@ -59,3 +79,21 @@ class outlook_email_sender():
 
     def display_email(self):
         self.mail_item.Display()
+
+
+def file_in_dir(path):
+    import os
+    
+    # folder path
+    dir_path = path
+
+    # list to store files
+    res = []
+
+    # Iterate directory
+    for path in os.listdir(dir_path):
+        # check if current path is a file
+        if os.path.isfile(os.path.join(dir_path, path)):
+            res.append(r'{}'.format(dir_path + "\\" + path))
+
+    return res
--- readme.txt
+++ readme.txt
@@ -5,6 +5,7 @@ Arguments:
     mail_subject - mail subject string
     mail_body - mail body if HTML them HTML should be true otherwise it will take the body as plain text
     mail_attachment - can give a list of path to add multiple files or can give a string to add a single file, default is None
+    mail_attachment_folder - can give a folder path to attach all the items in that folder to mail
     HTML - if the body text in html format, default is False
 
 Methods:
